353de1b9487cddc54fb729f09eaa8b3ea2082a31
tangtang <1987567948@qq.com>
2020-11-16 19:49:32 +0800

    修改：使用useState设置state值

--- src/EmojiGenerator.js
+++ src/EmojiGenerator.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 
 const emojiSet = ['🎃', '👻', '🧟', '😱', '👽', '💀'];
 
-class EmojiGenerator extends Component {
-    state = {
-        emoji: emojiSet[0]
-    }
+const EmojiGenerator = () => {
+    const [emoji, setEmoji] = useState(emojiSet[0]);
 
-    generateEmoji = () => {
+
+    const generateEmoji = () => {
         let randomIndex = Math.floor(Math.random() * emojiSet.length);
         let randomEmoji = emojiSet[randomIndex];
-
-        this.setState({
-            emoji: randomEmoji
-        })
+        setEmoji(randomEmoji);
     }
 
-    render() {
-        return (
-            <div>
-                <span className="emoji" role="img" aria-label="emoji">
-                    {this.state.emoji}
-                </span>
-                <button className="medium-btn" onClick={this.generateEmoji}>
-                    Generate
+    return (
+        <div>
+            <span className="emoji" role="img" aria-label="emoji">
+                {emoji}
+            </span>
+            <button className="medium-btn" onClick={generateEmoji}>
+                Generate
         </button>
-            </div>
-        )
+        </div>
+    )
 
-    }
 }
 
 class App extends Component {
